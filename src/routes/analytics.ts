85f8dedeccf58b292bade9d9e89d74360c475589
Aaditya Anand <aadityaanand271@gmail.com>
2025-02-06 17:00:37 -0500

    Updated the analysis routes

---
+++ src/routes/analytics.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import { getMeetingStats } from '../controllers/analytics';
+import { requireAuth } from '../middleware/auth';
+
+const router = express.Router();
+router.get('/stats', requireAuth, getMeetingStats);
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
 import { requireAuth } from './middleware/auth';
 import { log } from 'console';
-import meetingRoutes from './routes/meetings'
+import meetingRoutes from './routes/meetings';
+import analyticsRoutes from './routes/analytics';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
-app.use('/api/meetings', meetingRoutes)
+app.use('/api/meetings', meetingRoutes);
+app.use('/api/analytics', analyticsRoutes);
 app.get('/api/protected', requireAuth, (req, res)=>{
   res.json({message:`Hello User ${req.user!.userId}`});
 });
