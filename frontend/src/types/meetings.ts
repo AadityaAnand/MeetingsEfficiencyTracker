9cfff374e82519015898440b3ae249877a9e3a78
Aaditya Anand <aadityaanand271@gmail.com>
2025-02-10 16:22:53 -0500

    Added Main.css

---
+++ frontend/src/types/meetings.ts
@@ -0,0 +1,15 @@
+export interface Meeting{
+    _id: string;
+    title: string;
+    participants: string[];
+    duration: number;
+    efficiencyScore: number;
+    startTime: string;
+    outcomes: string;
+}
+
+export interface Stats {
+    totalMeetings: number;
+    averageDuration: number;
+    averageRfficiency: number;
+}
